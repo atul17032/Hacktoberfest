0b6f29c616a44cff5fd4b2bef27bbf942b9482bf
atul17032 <39928633+atul17032@users.noreply.github.com>
2020-10-07 19:50:17 +0530

    Add files via upload

---
+++ ass3.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<fcntl.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+int main(){
+	printf("Execution start\n");
+	printf("Enter the commands\n");
+	char buf[256];
+	ssize_t size=0;;
+	//int fd[2];
+	//pipe(fd);
+	while(1){
+		printf("sun\n");
+		int pid=fork();
+		if(pid==0){
+
+			//printf("devdhgew\n");
+			size= read(0,buf,256);
+			printf("%s  %d\n",buf ,size-1);
+			printf("sun %d\n",strcmp(buf, "exit\n") );
+			exit(0);
+		}
+		else if(pid>0){
+			//printf("sdwe\n");
+			wait(NULL);
+			printf("sunil %d\n",strcmp(buf, "exit\n") );
+			if(strcmp(buf, "exit\n") == 0){
+				break;
+			}
+			//printf("defcerfreer\n");
+		}
+		else{
+			printf("Failed to fork\n");
+		}
+	}
+	return 0;
+
+
+
+
+
+
+
+
+	/*char buf[5];
+	int fd[2];
+	pipe(fd);
+	int pid = fork();
+	if (pid == 0){
+		
+		write(fd[1], "hello", 5);
+
+	}
+	else{
+		wait();
+		read(fd[0], buf, 5); 
+		 // parent sends “hello”
+	}
+
+	printf("%s\n", buf);
+*/
+}
